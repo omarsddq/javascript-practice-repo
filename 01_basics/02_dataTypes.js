82eaf97c92a62012b476c43ab24f8b2d6a415cec
omarsddq <124886262+omarsddq@users.noreply.github.com>
2023-09-12 15:57:04 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs ,not browser
+
+//console.log(3 + 3); 
+// code readability a must
+
+
+let name = "omar"
+let age = 31
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
